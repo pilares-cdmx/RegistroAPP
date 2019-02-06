ef28f963a055bf7aee2c5c0fda5efa97ff0e9162
NumPoet <cbi2143044162@titlani.uam.mx>
2019-02-06 13:14:49 -0600

    Branch dedicada al desarrollo de un base de datos embebida que persista la información de registro de actividades

--- app/src/main/java/cdmx/sedu/registroapp_1/FullscreenActivity3.java
+++ app/src/main/java/cdmx/sedu/registroapp_1/FullscreenActivity3.java
@@ -1,16 +1,16 @@
 package cdmx.sedu.registroapp_1;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
+        import android.content.DialogInterface;
+        import android.content.Intent;
+        import android.support.v7.app.ActionBar;
+        import android.support.v7.app.AlertDialog;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.view.MotionEvent;
+        import android.view.View;
+        import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
