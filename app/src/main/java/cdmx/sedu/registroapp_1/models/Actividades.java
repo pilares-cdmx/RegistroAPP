9d8aa01aab21af95e2b3eb6d044f271db28df300
Sergio Negrete Villegas (numpoet) <bonekob2rt@gmail.com>
2019-02-12 02:00:50 -0600

    Agregando entidades-objeto

---
+++ app/src/main/java/cdmx/sedu/registroapp_1/models/Actividades.java
@@ -0,0 +1,7 @@
+package cdmx.sedu.registroapp_1.models;
+
+import io.realm.RealmObject;
+
+public class Actividades extends RealmObject {
+
+}
---
+++ app/src/main/java/cdmx/sedu/registroapp_1/models/Usuario.java
@@ -0,0 +1,7 @@
+package cdmx.sedu.registroapp_1.models;
+
+import io.realm.RealmObject;
+
+public class Usuario extends RealmObject {
+
+}
